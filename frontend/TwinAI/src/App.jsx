5b6734f53a88dd719325d039938611e4881e64d2
Suhas5459 <suhas5459jadhav@gmail.com>
2025-09-10 11:15:53 +0530

    done

--- frontend/TwinAI/src/App.jsx
+++ frontend/TwinAI/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login  from "../pages/login";
+import Login  from "../pages/Login";
 import Register from "../pages/Register";
 import PersonalityForm from "../pages/PersonalityForm";
 import ProtectedRoute  from "../../components/ProtectedRoute";
